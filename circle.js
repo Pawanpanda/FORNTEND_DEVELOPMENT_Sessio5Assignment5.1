e302ae2d47261be5ef6acc252acda4d6f8e806aa
Pawanpanda <30829150+Pawanpanda@users.noreply.github.com>
2017-08-18 14:41:58 +0530

    Update circle.js

--- circle.js
+++ circle.js
@@ -1,1 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
 
+<h2>Area</h2>
+
+<p>The area of the cicle is:</p>
+
+<p id="demo"></p>
+
+<script>
+function areaOfCircle(r,pi) { 
+	return pi*r*r;
+}
+document.getElementById("demo").innerHTML = areaOfCircle(5,3.14);
+</script>
+
+</body>
+</html>
